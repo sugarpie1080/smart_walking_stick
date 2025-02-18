36eabf774d06271f4db17a0f82832984d0fb0103
Felicity Lipscomb <2443084L@student.gla.ac.uk>
2025-02-18 17:22:14 +0000

    Added constructor and initalising function

--- drivers/src/sensors.cpp
+++ drivers/src/sensors.cpp
@@ -1,3 +1,25 @@
 #include <sensors.hpp>
 
-smart_stick::
+namespace smart_stick{
+    Sensor::Sensor(const char* chipname, int pin) : chipname(chipname), pin(pin), chip(nullptr)
+    {
+        // Open the GPIO Chip
+        chip = gpiod_chip_open(chipname);
+        // Check if can be opened
+        if (!chip) {
+            std::cerr << "Failed to open GPIO chip!" << std::endl;
+            exit(1);
+        }
+    }
+    
+    void Sensor::initialise()
+    {
+        line = gpiod_chip_get_line(chip, pin);
+        if (!line) {
+            std::cerr << "Failed to get GPIO line!" << std::endl;
+            gpiod_chip_close(chip);
+            exit(1);
+        }
+    }
+    
+}
