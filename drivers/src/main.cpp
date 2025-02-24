e80fe6af4fa7880de286f1b5d0d8a5167d0e4f31
Felicity Lipscomb <2443084L@student.gla.ac.uk>
2025-02-24 14:16:14 +0000

    Changed to interrupt pin

--- drivers/src/main.cpp
+++ drivers/src/main.cpp
@@ -3,7 +3,7 @@
 int main() {
     // Create an object inside the SensorLib namespace
     // smart_stick::ToFSensor tempSensor("/dev/gpiochip0", 16);
-    smart_stick::ToFSensor tempSensor("/dev/gpiochip0",16);
+    smart_stick::ToFSensor tempSensor("/dev/gpiochip0",17);
     // Initialize and start listening for interrupts
     tempSensor.initialise();
     tempSensor.startListening();
