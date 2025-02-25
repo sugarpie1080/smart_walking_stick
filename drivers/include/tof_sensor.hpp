b7ea998ff6c7f185493a73b8c132b3efb67a34b1
Felicity Lipscomb <2443084L@student.gla.ac.uk>
2025-02-25 07:57:08 +0000

    Added doxygen comments to ToFSensor

--- drivers/include/tof_sensor.hpp
+++ drivers/include/tof_sensor.hpp
@@ -1,3 +1,12 @@
+/**
+ * @file tof_sensor.hpp
+ * @author Felicity Lipscomb
+ * @brief Time of Flight sensor class
+ * @version 0.1
+ * @date 2025-02-25
+ * 
+ *  Driver copyright: Copyright (c) 2016 Pololu Corporation
+ */
 #ifndef TEST_SENSOR_H
 #define TEST_SENSOR_H
 
@@ -7,12 +16,35 @@
 
 namespace smart_stick
 {
+    /**
+     * @class TofSensor
+     * @brief Time of Flight Sensor Class
+     * 
+     */
     class ToFSensor : public Sensor
     {
         public:
+            /**
+             * @brief Construct a new ToFSensor object
+             * 
+             * @param chipname name of the GPIO chip used for the interrupt
+             * @param line pin number of interrupt pin
+             */
             ToFSensor(const char* chipname, int line);
+            /**
+             * @brief Initalises ToF sensor
+             * 
+             */
             void initialise() override; 
+            /**
+             * @brief Retrieves distance data from ToF sensor
+             * 
+             */
             void getData() override;
+            /**
+             * @brief Sends distance data
+             * 
+             */
             void send() override;
         private:
             VL53L0X sensor;
