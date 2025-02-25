b16b3b00e44bc071f1b5265bf945425222548020
Felicity Lipscomb <2443084L@student.gla.ac.uk>
2025-02-25 08:08:24 +0000

    Removed unused comments

--- drivers/src/main.cpp
+++ drivers/src/main.cpp
@@ -1,8 +1,7 @@
+
 #include <tof_sensor.hpp>
 
 int main() {
-    // Create an object inside the SensorLib namespace
-    // smart_stick::ToFSensor tempSensor("/dev/gpiochip0", 16);
     smart_stick::ToFSensor tempSensor("/dev/gpiochip0",17);
     // Initialize and start listening for interrupts
     tempSensor.initialise();
