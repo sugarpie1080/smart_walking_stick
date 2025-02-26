d3c686a34bfceddb6259fc9ed208c76791a30008
Felicity Lipscomb <2443084L@student.gla.ac.uk>
2025-02-26 13:31:27 +0000

    Include listener class for subscriber

---
+++ include/fastdds/SubListener.hpp
@@ -0,0 +1,46 @@
+#ifndef SUBLISTENER_HPP
+#define SUBLISTENER_HPP
+
+#include <fastdds/dds/subscriber/DataReaderListener.hpp>
+
+using namespace eprosima::fastdds::dds;
+
+
+template <typename T>
+class SubListener : public DataReaderListener
+    {
+    public:
+        SubListener() {};
+
+        ~SubListener() override {};
+
+        void on_subscription_matched(DataReader*,
+                const SubscriptionMatchedStatus& info) override
+        {
+            if (info.current_count_change == 1)
+            {
+                std::cout << "Subscriber matched." << std::endl;
+            }
+            else if (info.current_count_change == -1)
+            {
+                std::cout << "Subscriber unmatched." << std::endl;
+            }
+            else
+            {
+                std::cout << info.current_count_change
+                        << " is not a valid value for SubscriptionMatchedStatus current count change" << std::endl;
+            }
+        }
+
+	// callback
+        
+        // virtual void on_data_available(DataReader* reader)
+        // {
+        //     SampleInfo info;
+        //     T hello;
+        //     std::cout << "HHHHHHHHHHHHHHHHH" << std::endl;
+        // }
+
+    };
+
+#endif
