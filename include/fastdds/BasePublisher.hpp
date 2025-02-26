b04132f0e76e468dde81c03b79a91613806b3921
Felicity Lipscomb <2443084L@student.gla.ac.uk>
2025-02-26 14:39:39 +0000

    Added doxgen comments and reformatting

--- include/fastdds/BasePublisher.hpp
+++ include/fastdds/BasePublisher.hpp
@@ -32,83 +32,83 @@
 
 using namespace eprosima::fastdds::dds;
 
-   /**
-     * @brief Parent class for Publisher classes.
-     * 
-     * @tparam TTopicName Template parameter for the message used.
-     * @tparam TPubSubType Template parameter for the PubSubType.
-     */
-    template <typename TTopicName, typename TPubSubType>
-    class BasePublisher{
-        private:
-            DomainParticipant* participant_ = nullptr;
-            Publisher* publisher_ = nullptr;
-            Topic* topic_ = nullptr;
-            DataWriter* writer_ = nullptr;
-            TypeSupport type_;
-            PubListener listener_;
+/**
+ * @brief Parent class for Publisher classes.
+ * 
+ * @tparam TTopicName Template parameter for the message used.
+ * @tparam TPubSubType Template parameter for the PubSubType.
+ */
+template <typename TTopicName, typename TPubSubType>
+class BasePublisher{
+    private:
+        DomainParticipant* participant_ = nullptr;
+        Publisher* publisher_ = nullptr;
+        Topic* topic_ = nullptr;
+        DataWriter* writer_ = nullptr;
+        TypeSupport type_;
+        PubListener listener_;
 
-        public:
-            /**
-             * @brief Construct a new BasePublisher object
-             * 
-             * @param topic_name name of the topic.
-             */
-            BasePublisher(const std::string& topic_name):type_(new TPubSubType())
-            {
-                DomainParticipantQos participantQos;
-                participantQos.name("Participant_publisher");
-                participant_ = DomainParticipantFactory::get_instance()->create_participant(0, participantQos);
-                if (participant_ == nullptr)
-                    throw std::runtime_error("Failed to create DomainParticipant");
-        
-                type_.register_type(participant_);
-                topic_ = participant_->create_topic(topic_name,type_.get_type_name() , TOPIC_QOS_DEFAULT);
-                if (topic_ == nullptr)
-                    throw std::runtime_error("Failed to create Topic");
-                
-                publisher_ = participant_->create_publisher(PUBLISHER_QOS_DEFAULT, nullptr);
-                if (publisher_ == nullptr)
-                    throw std::runtime_error("Failed to create Publisher");
-                
-                writer_ = publisher_->create_datawriter(topic_, DATAWRITER_QOS_DEFAULT, &listener_);
-                if (writer_ == nullptr)
-                    throw std::runtime_error("Failed to create Reader");
-            }
-            }
-            /**
-             * @brief Destroy the BasePublisher object
-             * 
-             */
-            virtual ~BasePublisher()
-            {
-                if (writer_ != nullptr)
-                    publisher_->delete_datawriter(writer_);
-                if (publisher_ != nullptr)
-                    participant_->delete_publisher(publisher_);
-                if (topic_ != nullptr)
-                    participant_->delete_topic(topic_);
-                DomainParticipantFactory::get_instance()->delete_participant(participant_);
-            }
+    public:
+        /**
+         * @brief Construct a new BasePublisher object
+         * 
+         * @param topic_name name of the topic.
+         */
+        BasePublisher(const std::string& topic_name):type_(new TPubSubType())
+        {
+            DomainParticipantQos participantQos;
+            participantQos.name("Participant_publisher");
+            participant_ = DomainParticipantFactory::get_instance()->create_participant(0, participantQos);
+            if (participant_ == nullptr)
+                throw std::runtime_error("Failed to create DomainParticipant");
+    
+            type_.register_type(participant_);
+            topic_ = participant_->create_topic(topic_name,type_.get_type_name() , TOPIC_QOS_DEFAULT);
+            if (topic_ == nullptr)
+                throw std::runtime_error("Failed to create Topic");
             
-            /**
-             * @brief Publishes the message generated.
-             * 
-             * @param message message to be published.
-             * @return true message sent successfully.
-             * @return false message failed to send.
-             */
-            bool publish(TTopicName& message)
+            publisher_ = participant_->create_publisher(PUBLISHER_QOS_DEFAULT, nullptr);
+            if (publisher_ == nullptr)
+                throw std::runtime_error("Failed to create Publisher");
+            
+                writer_ = publisher_->create_datawriter(topic_, DATAWRITER_QOS_DEFAULT, &listener_);
+            if (writer_ == nullptr)
+                throw std::runtime_error("Failed to create Reader");
+        }
+        
+        /**
+         * @brief Destroy the BasePublisher object
+         * 
+         */
+        virtual ~BasePublisher()
+        {
+            if (writer_ != nullptr)
+                publisher_->delete_datawriter(writer_);
+            if (publisher_ != nullptr)
+                participant_->delete_publisher(publisher_);
+            if (topic_ != nullptr)
+                participant_->delete_topic(topic_);
+            DomainParticipantFactory::get_instance()->delete_participant(participant_);
+        }
+        
+        /**
+         * @brief Publishes the message generated.
+         * 
+         * @param message message to be published.
+         * @return true message sent successfully.
+         * @return false message failed to send.
+         */
+        bool publish(TTopicName& message)
+        {
+            if (listener_.matched_ > 0)
             {
-                if (listener_.matched_ > 0)
-                {
-                    writer_->write(&message);
-                    return true;
-                }
-                return false;
-            };
+                writer_->write(&message);
+                return true;
+            }
+            return false;
+        }
 
-    };
+};
 
 
 #endif //BASE_PUBLISHER_HPP
--- include/fastdds/BaseSubscriber.hpp
+++ include/fastdds/BaseSubscriber.hpp
@@ -1,7 +1,24 @@
-// GenericSubscriber.hpp
+/**
+ * @file BaseSubscriber.hpp
+ * @author Felicity Lipscomb
+ * @brief Parent Class for Subscriber classes.
+ * @version 0.1
+ * @date 2025-02-26
+ * 
+ * This file contains the parent class for Sublisher classes. 
+ * This uses templating to allow for different topics to be used,
+ * and to allow for different types of messages to be recieved. This hides 
+ * the boilerplate code from the application layer. 
+ * 
+ * @copyright Copyright 2016 Proyectos y Sistemas de Mantenimiento SL (eProsima).
+ * Referenced from FastDDS application tutorial.
+ */
 #ifndef BASE_SUBSCRIBER_HPP
 #define BASE_SUBSCRIBER_HPP
 
+// Subscriber listener includes
+#include <SubListener.hpp>
+// FastDDS includes
 #include <fastdds/dds/domain/DomainParticipant.hpp>
 #include <fastdds/dds/domain/DomainParticipantFactory.hpp>
 #include <fastdds/dds/subscriber/DataReader.hpp>
@@ -10,9 +27,14 @@
 #include <fastdds/dds/subscriber/Subscriber.hpp>
 #include <fastdds/dds/topic/TypeSupport.hpp>
 
-#include <SubListener.hpp>
 
 using namespace eprosima::fastdds::dds;
+/**
+ * @brief Parent class for Subscriber classes.
+ * 
+ * @tparam TTopicName Template parameter for the message used.
+ * @tparam TPubSubType Template parameter for the PubSubType.
+ */
 template <typename TTopicName, typename TPubSubType>
 class BaseSubscriber
 {
@@ -26,9 +48,18 @@ public:
     DataReader* reader_ = nullptr;
     SubListener<TTopicName> listener_;
 
+    /**
+     * @brief Construct a new BaseSubscriber object
+     * 
+     * @param topic_name name of the topic.
+     */
     BaseSubscriber(const std::string& topic_name) : type_(new TPubSubType()), topic_name(topic_name) 
     {}
-
+    
+    /**
+     * @brief Initialises the Subscriber.
+     * 
+     */
     void init()
     {
         DomainParticipantQos participantQos;
@@ -36,30 +67,25 @@ public:
         participant_ = DomainParticipantFactory::get_instance()->create_participant(0, participantQos);
     
         if (participant_ == nullptr)
-        {
             throw std::runtime_error("Failed to create DomainParticipant");
-        }
     
         type_.register_type(participant_);
-    
         topic_ = participant_->create_topic(topic_name, type_.get_type_name(),TOPIC_QOS_DEFAULT);
         if (topic_ == nullptr)
-        {
             throw std::runtime_error("Failed to create Topic");
-        }
     
         subscriber_ = participant_->create_subscriber(SUBSCRIBER_QOS_DEFAULT, nullptr);
         if (subscriber_ == nullptr)
-        {
             throw std::runtime_error("Failed to create Publisher");
-        }
     
         reader_ = subscriber_->create_datareader(topic_, DATAREADER_QOS_DEFAULT, &listener_);
         if (reader_ == nullptr)
-        {
             throw std::runtime_error("Failed to create Reader");
-        }
     }
+    /**
+     * @brief Destroy the BaseSubscriber object
+     * 
+     */
     virtual ~BaseSubscriber()
     {
         if (reader_ != nullptr)
