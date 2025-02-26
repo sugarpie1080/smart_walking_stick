81c0630f8f16e6b09b05a41542eb9b6df4997ad9
Felicity Lipscomb <2443084L@student.gla.ac.uk>
2025-02-26 14:08:33 +0000

    Added doxygen comments

--- include/fastdds/BasePublisher.hpp
+++ include/fastdds/BasePublisher.hpp
@@ -1,6 +1,17 @@
+/**
+ * @file BasePublisher.hpp
+ * @author Felicity Lipscomb
+ * @brief Parent Class for Publisher classes.
+ * @version 0.1
+ * @date 2025-02-26
+ * 
+ * @copyright Copyright 2016 Proyectos y Sistemas de Mantenimiento SL (eProsima).
+ * Referenced from FastDDS application tutorial.
+ * 
+ */
 #ifndef BASE_PUBLISHER_HPP
 #define BASE_PUBLISHER_HPP
-// Pub listender includes
+// Publisher listener includes
 #include <PubListener.hpp>
 // FastDDS includes
 #include <fastdds/dds/domain/DomainParticipant.hpp>
@@ -18,7 +29,13 @@ using namespace eprosima::fastdds::dds;
 
 namespace smart_stick 
 {
-    template <typename T, typename TPubSubType>
+    /**
+     * @brief Parent class for Publisher classes.
+     * 
+     * @tparam TTopicName Template parameter for the message used.
+     * @tparam TPubSubType Template parameter for the PubSubType.
+     */
+    template <typename TTopicName, typename TPubSubType>
     class BasePublisher{
         private:
             DomainParticipant* participant_ = nullptr;
@@ -29,9 +46,14 @@ namespace smart_stick
             PubListener listener_;
 
         public:
+            /**
+             * @brief Construct a new BasePublisher object
+             * 
+             * @param topic_name name of the topic
+             */
             BasePublisher(const std::string& topic_name):type_(new TPubSubType());
             virtual ~BasePublisher();
-            bool publish(T& message);
+            bool publish(TTopicName& message);
 
     };
 }
