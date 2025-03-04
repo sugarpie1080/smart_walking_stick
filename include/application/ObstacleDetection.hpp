53584916161b3f53b1fdfc436343798e3b2d3eaf
Felicity Lipscomb <2443084L@student.gla.ac.uk>
2025-03-04 17:27:34 +0000

    Added publisher

--- include/application/ObstacleDetection.hpp
+++ include/application/ObstacleDetection.hpp
@@ -2,49 +2,57 @@
 #ifndef OBSTACLE_DETECTION_HPP
 #define OBSTACLE_DETECTION_HPP
 
-// Publisher Include
-#include <BasePublisher.hpp>
-// Subscriber Include
+// Subscriber includes
 #include <BaseSubscriber.hpp>
 #include <SubListener.hpp>
 #include <fastdds/dds/subscriber/SampleInfo.hpp>
-// Message Types Include
-#include <ToFDataPubSubTypes.h>
-#include <MotorCommandPubSubTypes.h>
 
-#endif // OBSTACLE_DETECTION_HPP
+// Topic includes
+#include <ToFDataPubSubTypes.h>
+
+using namespace eprosima::fastdds::dds;
 
 namespace smart_stick {
-
-    class ObstacleDetection
-    {
+/**
+ * @brief Example Subscriber class for ToFData messages. Derived from the BasePublisher Class.
+ * 
+ * To use the base example, you must declare the class 
+ * with the message type and the PubSubType.
+ */
+class ObstacleDetectionSub : public BaseSubscriber<ToFData, ToFDataPubSubType> {
+protected:
+    /**
+     * @brief Example Listener class for ToFData messages. Derived from the SubListener Class.
+     * 
+     * A listener class is required for every subscriber. This class contains the callback
+     * function for the subscriber, which determines what actions are taken with the message.
+     * 
+     */
+    class OSSubListener : public SubListener<ToFData> {
     public:
-        // Subscriber class for ToFData messages.
-        class ObstacleDetectionSub: BaseSubscriber<ToFData, ToFDataPubSubType>;
-        // Publisher class for MotorCommand messages.
-        class ObstacleDetectionPub: BasePublisher<MotorCommand, MotorCommandPubSubType>;
-        int convert_distance_to_duty_cycle(float distance);   
+        /**
+         * @brief Callback function for the subscriber.
+         * 
+         * @param reader FastDDS DataReader object.
+         */
+        void on_data_available(DataReader* reader) override;
     };
 
-    class ObstacleDetection::ObstacleDetectionSub
-    {
-        protected:
-            class ObstacleDetectionListener: SubListener<ToFData>
-            {
-                public:
-                    void on_data_available(DataReader* reader) override;
-            };
-            ObstacleDetectionListener listener_;
-        public:
-            ObstacleDetectionSub();
-            void set_listener(DataReader* reader);
-    };
+    OSSubListener listener_;
 
-    class ObstacleDetection::ObstacleDetectionPub
-    {
-        public:
-            ObstacleDetectionPub();
-            void publish_message(MotorCommand message);
-    };
+public:
+    /**
+     * @brief Construct a new ToFDataSubscriber object
+     * 
+     */
+    ObstacleDetectionSub();
+    /**
+     * @brief Sets the listener object for the subscriber.
+     * 
+     * @param reader FastDDS DataReader object.
+     */
+    void set_listener(DataReader* reader);
+};
+}
 
-    }
+#endif // TOF_DATA_SUBSCRIBER_HPP
