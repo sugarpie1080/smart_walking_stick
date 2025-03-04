b00966732177ad5477f7ccee3146215567af74c7
BaileyHodgson <baileyhodgson3@gmail.com>
2025-03-04 16:11:28 +0000

    formatted message to include timestamp and distance

--- src/drivers/sensors/tof_sensor.cpp
+++ src/drivers/sensors/tof_sensor.cpp
@@ -11,6 +11,14 @@
 #include <tof_sensor.hpp>
 // Driver Includes
 #include <VL53L0X.hpp>
+#include <iostream>
+#include <ctime>
+#include <chrono>
+#include <functional>
+#include <memory>
+#include <string>
+#include "rclcpp/rclcpp.hpp"
+#include "std_msgs/msg/string.hpp"
 
 
 namespace smart_stick 
@@ -39,4 +47,32 @@ namespace smart_stick
     void ToFSensor::send() {
         std::cout << "Sending Distance data..." << std::endl;
     }
+
+    class tofPub : public rclcpp::None
+    {
+        public:
+        tofPub()
+        : Node("tof_publisher"), count_(0)
+        {
+            publisher = this->create_publisher<std_msgs::msg::String>("topic", 10);
+            timer_ = this->create_wall_timer(
+                500ms, std::bind(&tofPub::timer_callback, this));
+        }
+
+        private:
+        void timer_callback()
+        {
+            auto message = std_msgs::msg::String();
+
+            std::time_t result = std::time(nullptr);
+            std::cout << std::asctime(std::localtime(&result)) << result << " seconds since the Epoch\n";
+
+            message.data = std::to_string(count_++) + result + getData();
+            RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str());
+            publisher_->publish(message);
+        }
+        rclcpp::TimerBase::SharedPtr timer_;
+        rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
+        size_t count_;
+    };
 }
