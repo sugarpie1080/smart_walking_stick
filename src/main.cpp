7d643c9759cf341819b94d95a241b82a49a907f6
Felicity Lipscomb <2443084L@student.gla.ac.uk>
2025-03-10 19:41:09 +0000

    Using correct class name

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@ int main() {
     const char* chipname = "/dev/gpiochip0";  // Adjust based on hardware
     int line = 17; // Adjust GPIO pin number accordingly
 
-    smart_stick::ToFPublisher tof_publisher(chipname, line);
+    smart_stick::ToFDataPublisher tof_publisher(chipname, line);
     tof_publisher.initialise();
 
     std::cout << "ToF Publisher running..." << std::endl;
