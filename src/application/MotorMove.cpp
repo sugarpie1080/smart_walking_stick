3e624ce7eb2c5dbd84d5a61ae42f68c96d30da2c
Felicity <2443084L@student.gla.ac.uk>
2025-03-12 12:29:10 +0000

    Added motor movement

--- src/application/MotorMove.cpp
+++ src/application/MotorMove.cpp
@@ -4,7 +4,24 @@
 namespace smart_stick {
 
 
-MotorMove::MotorMove() : BaseSubscriber("MotorCommandsTopic") {}
+MotorMove::MotorMove() : BaseSubscriber("MotorCommandsTopic") 
+{
+    chip = gpiod_chip_open(chipname);
+    // Check if can be opened
+    if (!chip) {
+        std::cerr << "Failed to open GPIO chip!" << std::endl;
+        exit(1);
+    }
+    // Get the GPIO line
+    line = gpiod_chip_get_line(chip, pin);
+    if (!line) {
+        std::cerr << "Failed to get GPIO line!" << std::endl;
+        gpiod_chip_close(chip);
+        exit(1);
+    }
+
+    int ret = gpiod_line_request_output(line,"Consumer",0);
+}
 
 void MotorMove::set_listener(DataReader* reader) {
     reader->set_listener(&listener_);
@@ -12,6 +29,7 @@ void MotorMove::set_listener(DataReader* reader) {
 
 // Callback implementation
 void MotorMove::MotorMoveListener::on_data_available(DataReader* reader) {
+    struct gpiod_line* line = parent_->getLine();
     SampleInfo info;
     MotorCommands message;
     if (reader->take_next_sample(&message, &info) == ReturnCode_t::RETCODE_OK) {
@@ -19,6 +37,7 @@ void MotorMove::MotorMoveListener::on_data_available(DataReader* reader) {
            if (message.duty_cycle() == 100) {
                 // To be Changed with the actual motor command
                std::cout << "Full Power!" << std::endl;
+            //    gpiod_line_set_value(1); 
            } else {
                 // To be Changed with the actual motor command
                std::cout << "Motor Off" << std::endl;
--- src/drivers/sensors/sensors.cpp
+++ src/drivers/sensors/sensors.cpp
@@ -20,6 +20,10 @@ namespace smart_stick{
             std::cerr << "Failed to open GPIO chip!" << std::endl;
             exit(1);
         }
+        else 
+        {
+            std::cout << "Chip opened" << std::endl;
+        }
         // Get the GPIO line
         line = gpiod_chip_get_line(chip, pin);
         if (!line) {
