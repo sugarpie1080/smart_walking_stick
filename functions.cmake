18cc861d18d078870a35bb63759e74d3b9bf46a3
Felicity <2443084L@student.gla.ac.uk>
2025-04-05 16:29:50 +0100

    Changed to use duty cycle

--- functions.cmake
+++ functions.cmake
@@ -1,8 +1,6 @@
 set_property(GLOBAL PROPERTY TOPICS_DIR)
 function(create_msg_library msg_name)
-    # set(tmp "${CMAKE_CURRENT_SOURCE_DIR}/src/topics")
-    # set(MSG_CXX_FILE "${TOPICS_DIR}/${msg_name}.cxx")
-    # set(MSG_PUBSUB_FILE "${TOPICS_DIR}/${msg_name}PubSubTypes.cxx")
+
     file(GLOB MSG_FILES "${TOPICS_DIR}/${msg_name}*.cxx")
     message(INFO  "${MSG_FILES}")
     execute_process(
